4774230bf259a66dd508dcc45f502cc6b5b2f615
fhideous <kul.leshiy@yandex.ru>
2020-12-09 17:44:39 +0300

    percant and backslash handling

--- ft_printf.c
+++ ft_printf.c
@@ -48,13 +48,50 @@ void print_oper (s_operation oper)
 	printf("flag.i_zer: %d\n",oper.flag.is_zero);
 	printf("======================\n\n");
 }
+int print_backslash(char** str)
+{
+	if (*(*str) == '\\')
+		write(1, &"\\", 1);
+	if (**str == '"')
+		write(1, &"\"", 1);
+//	if (**str != '\\' || **str != '"' || **str != '%')
+	if (*(*str + 1) == '%')
+		write(1, &"%", 1);
+	(*str)++;
+	return (1);
+}
+
+int print_percent (char** str)
+{
+	if (*(*str + 1) == '%')
+	{
+		*(*str)++;
+		*(*str)++;
+		write(1, &"%", 1);
+		return (1);
+	}
+	return (0);
+}
+
 int print_line(char** str)
 {
 	int i;
 
 	i = 0;
-	while(**str != '\0' && **str != '%')
+	while(**str != '\0')
 	{
+		if (**str == '\\')
+			i += print_backslash(&*str);
+		if (**str == '%')
+		{
+			if (print_percent(&*str))
+			{
+				i++;
+				continue;
+			}
+			else
+				return (i);
+		}
 		if (!(write(1, *str, 1)))
 			return (-1);
 		i++;
@@ -62,6 +99,7 @@ int print_line(char** str)
 	}
 	return (i);
 }
+
 
 unsigned int check_flag(char **str, s_operation *oper)
 {
@@ -124,7 +162,6 @@ unsigned int check_oper(char** str, s_operation *oper)
 {
 	unsigned int i;
 	i = 0;
-	//(*str)++;
 	i += check_flag(&*str, *&oper);
 	i += check_width(&*str, *&oper);
 	if (**str == '/')
