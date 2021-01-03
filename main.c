e05245f065e6d6af09b7b6064bbf14058e2eeabc
fhideous <kul.leshiy@yandex.ru>
2021-01-03 19:52:25 +0300

    add test functions for %p

--- main.c
+++ main.c
@@ -173,6 +173,45 @@ void check_orig_char (int arg, char **strs, int len)
 	}
 }
 
+
+
+void check_my_ptr (void *arg, char **strs, int len)
+{
+
+	int j = 0;
+	ft_printf("==================================");
+	ft_printf("\n=======       %-14p=====\n",arg);
+	ft_printf("==================================\n");
+	while (j < len)
+	{
+		//	ft_printf("\nmy:\n");
+		ft_printf("%s:\t", strs[j]);
+		int i =  ft_printf (strs[j], arg);
+		ft_printf("\t return: %d\n", i);
+		//*strs++;
+		j++;
+		//	printf ("%s", *strs);
+	}
+}
+
+void check_orig_ptr (void *arg, char **strs, int len)
+{
+	int j = 0;
+	printf("==================================");
+	printf("\n=======       %-14p=====\n",arg);
+	printf("==================================\n");
+	while (j < len)
+	{
+		//	printf("\noriginal:\n");
+		printf("%s:\t", strs[j]);
+		int i = printf (strs[j], arg);
+		printf("\t return: %d\n", i);
+		j++;
+
+	}
+}
+
+
 char** line_for_check(char **line, int type, int line_len)
 {
 	char **str = calloc (line_len, sizeof(char*));
@@ -186,6 +225,8 @@ char** line_for_check(char **line, int type, int line_len)
 	free(str_add);
 	return (str);
 }
+
+
 
 
 //unsigned int take_address(int a)
@@ -220,90 +261,72 @@ int main(void)
 			"%9",	"%09","%01", "%04", "%00", "%-9", "%-.9",
 			"%-.0", "%-0.0" ,  "%05.9", "%09.5",
 			"%-05.9", "%5.3", "%-5.3", "%-09.5", "%-9.5", "%-05.9",
-			"%00.0", "%5.3", "%0.2", "%02", "%3.0", "%05.5"
+			"%00.0", "%5.3", "%0.2", "%02", "%3.0", "%05.5", "%0.10", "%.10"
 	};
-
 	char *check54[] = {"%1.0"};
-
 
 	int a_len = sizeof(a) / sizeof(a[0]);
 	int len_check = sizeof(check) / sizeof(*check);
-
 
 	char **int_check = line_for_check(check, 'd', len_check);
 	char **str_check = line_for_check(check, 's', len_check);
 	char **char_check = line_for_check(check, 'c', len_check);
 	char **un_check = line_for_check(check, 'u', len_check);
+	char **ptr_check = line_for_check(check, 'p', len_check);
 //
 	char *b[] = {"He", ",", "Hello ", "world ", "Hello world", "", " ", "!"};
 	char *b1[] = {"!"};
-	char c [] = {'a', 'b', 'c', 'd', '\t', '%',
-			  '\n'};
+	char c [] = {'a', 'b', 'c', 'd', '\t', '%', '\n'};
+
 	char c1 [] = {'c'};
 	unsigned int d[] = {0, 10, 245, 2147483647, 2147483649, 4294967295,
 					 429496726, 4294967296};
 	unsigned int d1[] = {0};
 
-	//	unsigned int a[a_len];
+	unsigned int e[] = {&d[0], &a[0], &c[0], &b[4] };
 
-//	i = 0;
-//	while (i < a_len)
-//	{
-//		take_address(a, a_len);
-//		i++;
-//	}
-		int b_len = sizeof(b) / sizeof(b[0]);
+	int b_len = sizeof(b) / sizeof(b[0]);
 	int c_len = sizeof (c) / sizeof(c[0]);
 	int d_len = sizeof (d) / sizeof(d[0]);
-//	i = 10;
-//	unsigned int p;
-//	p = 15;	printf ("%x\n", p);
-//	p = 19;	printf ("%x\n", p);
-//	p = 23;		printf ("%x\n", p);
-//	p = 24;		printf ("%x\n", p);
-//	p = 25;		printf ("%x\n", p);
-//	p = 26;		printf ("%x\n", p);
-//	char *stwt;
-//	dectox_int(19, stwt);
-
+	int e_len = sizeof (e) / sizeof(e[0]);
 
 //////////////				MY
-	i = -1;
-	while (++i < a_len)
-		check_my(a[i], int_check, len_check);
+//
+//	printf("%p", &i);
 
+//
+//	i = -1;
+//	while (++i < a_len)
+//		check_my(a[i], int_check, len_check);
+//
+//	i = -1;
+//	while (++i < b_len)
+//		check_my_str(b[i], str_check, len_check);
+//	i = -1;
+//	while (++i < c_len)
+//		check_my_char(c[i], char_check, len_check);
+//	i = -1;
+//	while (++i < d_len)
+//		check_un_my(d[i], un_check, len_check);
 	i = -1;
-	while (++i < b_len)
-		check_my_str(b[i], str_check, len_check);
-	i = -1;
-	while (++i < c_len)
-		check_my_char(c[i], char_check, len_check);
-	i = -1;
-	while (++i < d_len)
-		check_un_my(d[i], un_check, len_check);
+	while (++i < e_len)
+		check_my_ptr(e[i], ptr_check, len_check);
 
-
-/////////////			Original
-
+///////////////			Original
+//
+//	i = -1;
+//	while (++i < a_len)
+//		check_orig(a[i], int_check, len_check);
+//	i = -1;
+//	while (++i < b_len)
+//		check_orig_str(b[i], str_check, len_check);
+//	i = -1;
+//	while (++i < c_len)
+//		check_orig_char(c[i], char_check, len_check);
+//	i = -1;
+//	while (++i < d_len)
+//		check_un_orig(d[i], un_check, len_check);
 	i = -1;
-	while (++i < a_len)
-		check_orig(a[i], int_check, len_check);
-	i = -1;
-	while (++i < b_len)
-		check_orig_str(b[i], str_check, len_check);
-	i = -1;
-	while (++i < c_len)
-		check_orig_char(c[i], char_check, len_check);
-	i = -1;
-	while (++i < d_len)
-		check_un_orig(d[i], un_check, len_check);
+	while (++i < e_len)
+		check_orig_ptr(e[i], ptr_check, len_check);
 }
-
-
-
-
-
-
-
-
-
